263dd585d1cae5b504deccb21553924253af6a26
ningshengit <3229225035@qq.com>
2021-03-16 20:12:19 +0800

    0.3-文章模型创建

---
+++ Myblog-0.3-文章模型创建/app/faker.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2021-03-15 22:42:03
+# @Author  : autohe (${email})
+# @知乎    : https://www.zhihu.com/people/kuanye
+# @微信    : xdxh-1
+# @funtion : 制造假数据
+# @Version : $Id$
+
+import random
+from faker import Faker
+from sqlalchemy.exc import IntegrityError
+from app.extensions import db
+from app.models import Admin, Tags, Post, Comment
+
+fake = Faker()
+
+
+def fake_admin():
+    admin = Admin(
+        username='autohe',
+        master='autohe',
+        about='fack day...'
+    )
+    admin.set_password('1')
+    db.session.add(admin)
+    db.session.commit()
+
+
+def fake_tags(count=10):
+    tag = Tags(name='Default')
+    db.session.add(tag)
+
+    for i in range(count):
+        tag = Tags(name=fake.word())
+        db.session.add(tag)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
+def fake_posts(count=50):
+    for i in range(count):
+        post = Post(
+            title=fake.sentence(),
+            body=fake.text(2000),
+            tag=Tags.query.get(random.randint(1, Tags.query.count())),
+            timestamp=fake.date_time_this_year()
+        )
+
+        db.session.add(post)
+    db.session.commit()
+
+
+def fake_comments(count=500):
+    for i in range(count):
+        comment = Comment(
+            author=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+    salt = int(count * 0.1)
+    for i in range(salt):
+        # unreviewed comments
+        comment = Comment(
+            author=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=False,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+        # from admin
+        comment = Comment(
+            author='autohe',
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            from_admin=True,
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+    # replies
+    for i in range(salt):
+        comment = Comment(
+            author=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            replied=Comment.query.get(random.randint(1, Comment.query.count())),
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
